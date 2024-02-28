fb67e424bc35cd9788950e34c4a5a5dfae462a86
tiagoBatschke <83837600+tiagoBatschke@users.noreply.github.com>
2024-02-28 17:59:51 -0300

    testando

--- VitacomBack/config/cors.php
+++ VitacomBack/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'], 
+    'allowed_origins' => ['https://testevitacon-bd7d417ef875.herokuapp.com/'], 
 
     'allowed_origins_patterns' => [],
 
