8d99e82757948b382026c95fee96033119d6f83b
tiagoBatschke <83837600+tiagoBatschke@users.noreply.github.com>
2024-03-06 22:32:16 -0300

    corrigindo clientes

--- VitacomBack/config/cors.php
+++ VitacomBack/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://main--vitacontestefront.netlify.app', 'http://localhost:5173'], 
+    'allowed_origins' => ['https://vitacontestefront.netlify.app/', 'http://localhost:5173'], 
 
     'allowed_origins_patterns' => [],
 
