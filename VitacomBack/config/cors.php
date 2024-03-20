9a20a885b65f1bd5ec6e1e583dab98b8ae2eb226
tiagoBatschke <83837600+tiagoBatschke@users.noreply.github.com>
2024-03-20 10:19:58 -0300

    adicionei o crud de diferenciais e poi

--- VitacomBack/config/cors.php
+++ VitacomBack/config/cors.php
@@ -23,7 +23,7 @@ return [
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['Access-Control-Allow-Origin', 'Access-Control-Allow-Headers', 'Access-Control-Allow-Methods', 'Access-Control-Allow-Credentials'],
 
     'exposed_headers' => ['Access-Control-Allow-Origin', 'Access-Control-Allow-Headers', 'Access-Control-Allow-Methods', 'Access-Control-Allow-Credentials'],
 
